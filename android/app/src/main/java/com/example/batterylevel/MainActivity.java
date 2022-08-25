56ba83c139e6155c9900ec3897402a14d5b77b53
Siriwika <sirip571@epicc.postbox.in.th>
2022-08-25 14:09:32 +0700

    config firebase firestore

--- android/app/src/main/java/com/example/batterylevel/MainActivity.java
+++ android/app/src/main/java/com/example/batterylevel/MainActivity.java
@@ -1,6 +1,56 @@
 package com.example.batterylevel;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import android.util.Log;
+//import android.os.Bundle;
+
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+    private static final String TAG = "MainActivity";
+    // Access a Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                           db.collection("User")
+                                    .get().addOnCompleteListener(task -> {
+                                         if(task.isSuccessful()){
+
+                                             if (task.getResult().isEmpty()) {
+                                                 System.out.println("tasks is empty");
+                                             }
+                                             for (QueryDocumentSnapshot doc : task.getResult()){
+                                                 Log.d(TAG, "configureFlutterEngine: "+doc);
+                                                 Log.d(TAG, doc.getId()+ " => " +doc.getData());
+                                             }
+                                         }else{
+                                             Log.w(TAG, "Error getting documents.", task.getException());
+                                         }
+                                    });
+                            // This method is invoked on the main thread.
+                            if (call.method.equals("getBatteryLevel")) {
+//                                List<String> dataList = new ArrayList<String>();
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("name","Cake");
+                                user.put("age","22");
+                                result.success(user);
+                        }
+                        }
+                );
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -9,21 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,38 +24,43 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class User{
+  User({
+    required  this.name,
+    required this.age
+});
+  String name;
+  int age;
 
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   static const platform = MethodChannel('samples.flutter.dev/battery');
-  // Get battery level.
-  String _batteryLevel = 'Unknown battery level.';
+
+// Get battery level.
+  late User _user =  User(name: "", age:0);
 
   Future<void> _getBatteryLevel() async {
-    String batteryLevel;
+
     try {
-      final int result = await platform.invokeMethod('getBatteryLevel');
-      batteryLevel = 'Battery level at $result % .';
+      final result = await platform.invokeMethod('getBatteryLevel');
+      _user.name = result['name'];
+      // print(result);
+      // print(result['age']);
+      // print(result.runtimeType);
+
     } on PlatformException catch (e) {
-      batteryLevel = "Failed to get battery level: '${e.message}'.";
+      _user.name = "Failed to get battery level: '${e.message}'.";
     }
 
     setState(() {
-      _batteryLevel = batteryLevel;
+      _user = _user;
     });
   }
 
@@ -78,14 +73,12 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             ElevatedButton(
               onPressed: _getBatteryLevel,
-              child: const Text('Get Battery Level'),
+              child: const Text('Get Name'),
             ),
-            Text(_batteryLevel),
+            Text(_user.name.toString()),
           ],
         ),
       ),
     );
   }
-
-
 }
